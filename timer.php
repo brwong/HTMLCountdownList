0e4a96dce59b1f2be21bbd2160a23550a93658d7
Brandon Wong <projects@brwong.net>
2012-02-16 04:27:50 -0500

    new version
    index file calls timer which reads dates
    index then updates every second

---
+++ timer.php
@@ -0,0 +1,20 @@
+<?php
+	//$date = Array(2012,2,15,23,12,31,20);
+	$string = "";
+	$string .= file_get_contents("important.dates");
+
+	$contents = explode("\n", $string);
+
+	$dates = Array();
+
+	foreach($contents as $line){
+		$event = explode("\t", $line);
+		if(isset($event[1])){
+			$dates[$event[0]] = strtotime($event[1]);
+			//echo date("Y-m-d H:i:s", strtotime($event[1]))."<br />";
+		}
+		unset($event);
+	}
+
+	echo json_encode($dates);
+?>
